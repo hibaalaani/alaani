570a1312977dba6f47746bf519f318c18cb6242b
hiba <>
2024-10-06 14:19:13 +0200

    fix the basename

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+ 
   base: '/alaani/',  // Set this to match your repository name
 })
